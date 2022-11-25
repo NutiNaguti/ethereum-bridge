939cf266a1d85559a69959a74564460904a5ef43
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-25 16:24:34 +0400

    Added experemental events, redeployed with new addresses

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Ethereum <> NEAR Protocol
 -------------------------
 
-FunCoin goerli address: 0xE290a335175ACa1085fDd62008B03B93FA7Df4B5 
-Bridge goerli address:  0x64A24042D15F33fE8fDF2A9dc8030503AE251c80
+FunCoin goerli address: 0xbEF881280a8291253Bb116AeA2b04b9777Ff0BBC 
+
+Bridge goerli address:  0xd3D992616A88154f86B326D0752C44685838e3E9
--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -11,13 +11,22 @@ contract Bridge {
         uint256 indexed amount
     );
 
+    event Locked2(uint256 blockNumber, address account, uint256 amount);
+
+    event Locked3(uint256, address, uint256);
+
     constructor(address funCoin) {
         _funCoin = IFunCoin(funCoin);
     }
 
     function lock(uint256 amount) external returns (uint256, address, uint256) {
         _funCoin.transferFrom(msg.sender, address(this), amount);
+        // Indexed and anonymous args of event decodes differently
+        // https://docs.soliditylang.org/en/latest/abi-spec.html#abi-events
+        // https://docs.soliditylang.org/en/latest/contracts.html#events
         emit Locked(block.number, msg.sender, amount);
+        emit Locked2(block.number, msg.sender, amount);
+        emit Locked3(block.number, msg.sender, amount);
         return (block.number, msg.sender, amount);
     }
 }
