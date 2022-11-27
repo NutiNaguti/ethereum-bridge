9dc0c9a8deec5a7998fd6dfe4636e3058f98150f
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-27 16:48:01 +0400

    changed proof event

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5,28 +5,19 @@ import "contracts/interfaces/IFunCoin.sol";
 contract Bridge {
     IFunCoin private _funCoin;
 
-    event Locked(
-        uint256 indexed blockNumber,
-        address indexed account,
-        uint256 indexed amount
-    );
-
-    event Locked2(uint256 blockNumber, address account, uint256 amount);
-
-    event Locked3(uint256, address, uint256);
+    event Locked(bytes32 indexed proof);
 
     constructor(address funCoin) {
         _funCoin = IFunCoin(funCoin);
     }
 
-    function lock(uint256 amount) external returns (uint256, address, uint256) {
+    function lock(uint256 amount) external returns (bytes32 proof) {
         _funCoin.transferFrom(msg.sender, address(this), amount);
         // Indexed and anonymous args of event decodes differently
         // https://docs.soliditylang.org/en/latest/abi-spec.html#abi-events
         // https://docs.soliditylang.org/en/latest/contracts.html#events
-        emit Locked(block.number, msg.sender, amount);
-        emit Locked2(block.number, msg.sender, amount);
-        emit Locked3(block.number, msg.sender, amount);
-        return (block.number, msg.sender, amount);
+        proof = keccak256(abi.encode(block.number, msg.sender, amount));
+        emit Locked(proof);
+        return proof;
     }
 }
